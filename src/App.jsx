36d26213d6a979b83ae1785ea962101d34155769
Brendan <pidouxbrendan@gmail.com>
2025-04-29 11:40:12 +0200

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import Main from './components/Main';
+
+
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    // Remplacez l'URL par celle de votre Strapi local
-    fetch('http://localhost:1337/api/posts') 
-      .then((response) => response.json())
-      .then((data) => setPosts(data.data));
-  }, []);
-
   return (
-    <div className="container mx-auto p-4">
-      <p></p>
-      <h1 className="text-3xl font-bold">Posts</h1>
-      <div>
-        {posts.map((post) => (
-          <div key={post.id} className="p-4 border-b">
-            <h2 className="text-xl">{post.attributes.title}</h2>
-            <p>{post.attributes.description}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    <Main/>
+  )
 }
+//function App() {
+//  const [posts, setPosts] = useState([]);
+//
+//  useEffect(() => {
+//    // Remplacez l'URL par celle de votre Strapi local
+//    fetch('http://localhost:1337/api/posts') 
+//      .then((response) => response.json())
+//      .then((data) => setPosts(data.data));
+//  }, []);
+//
+//  return (
+//    <div className="container mx-auto p-4">
+//      <p></p>
+//      <h1 className="text-3xl font-bold">Posts</h1>
+//      <div>
+//        {posts.map((post) => (
+//          <div key={post.id} className="p-4 border-b">
+//            <h2 className="text-xl">{post.attributes.title}</h2>
+//            <p>{post.attributes.description}</p>
+//          </div>
+//        ))}
+//      </div>
+//    </div>
+//  );
+//}
 
 export default App;
 
--- src/components/Inscription.jsx
+++ src/components/Inscription.jsx
@@ -25,29 +25,29 @@ function Inscription() {
     return (
         <div className="min-h-screen flex items-center justify-content bg-gray-100">
             <form onSubmit={handleInscription} className="bg-white p-8 rounded-lg shadow-md w-96">
-                <h2 className="text-2x1 font-bold mb-6 text-center">Créer un compte</h2>
+                <h2 className="text-2xl font-bold mb-6 text-center text-black">Créer un compte</h2>
                 <input 
                     type="email"
                     placeholder="Email"
-                    className="w-full p-2 mb-4 border rounded"
+                    className="w-full p-2 mb-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-orange-400"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
                 <input 
                     type="text"
                     placeholder="Nom d'utilisateur"
-                    className="w-full p-2 mb-4 border rounded"
+                    className="w-full p-2 mb-4 border border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-orange-400"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                 />
                 <input 
                     type="password"
                     placeholder="Mot de passe"
-                    className="w-full p-2 mb-6 border rounded"
+                    className="w-full p-2 mb-6 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-orange-400"
                     value={password} 
                     onChange={(e) => setPassword(e.target.value)}
                 />
-                <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition duration-200">
+                <button type="submit" className="w-full bg-orange-500 text-white p-2 rounded hover:bg-orange-600 transition duration-200">
                     S'inscrire
                 </button>
 
