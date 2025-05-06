7b92cdce80d362816397a5ffc64b5e837edb0475
Brendan <pidouxbrendan@gmail.com>
2025-05-06 10:32:30 +0200

    ajout notif create post dans navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 import "../Navbar.css"
 
 function Navbar() {
+    const [showNotifications, setShowNotifications] = useState(false);
+    const [showCreateMenu, setShowCreateMenu] = useState(false);
+
     return (
         <header className="navbar">
             <div className="navbar-left">
@@ -13,8 +17,26 @@ function Navbar() {
             </div>
 
             <div className="navbar-right">
+                <div className="icon-wrapper" onClick={() => setShowNotifications(!showNotifications)}>
+                    <i className="bi bi-bell-fill"></i>
+                    {showNotifications && (
+                        <div className="dropdown">
+                            <p>Aucune notification</p>
+                        </div>
+                    )}
+                </div>
+                <div className="create-btn" onClick={() => setShowCreateMenu(!showCreateMenu)}>
+                    <i className="bi bi-plus-lg"></i> <span>Create</span>
+                    {showCreateMenu && (
+                        <div className="dropdown">
+                            <p>Nouveau post</p>
+                            <p>Importer une image</p>
+                        </div>
+                    )}
+
+                </div>
                 <Link to={"/connexion"}>
-                <button className="login-btn">Connexion</button>
+                    <button className="login-btn">Connexion</button>
                 </Link>
             </div>
         </header>
