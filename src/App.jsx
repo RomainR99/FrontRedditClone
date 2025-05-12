e66991528e0f065d4d93da2963adb5c2a1ac855b
Ethan Pandor <ethanpandor43@gmail.com>
2025-05-12 15:51:56 +0200

    correctif page loggin

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Login from './components/Login';
+import Main from './components/Main';
+import SignUp from './components/SignUp';
+import Posts from './components/Posts';
+import FirstLogin from './components/FirstLogin';
+import CreatePost from './components/CreatePost';
+import GoogleCallback from './components/GoogleCallback';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/first-login" element={<FirstLogin />} />
+        <Route path="/createPost" element={<CreatePost />} />
+        <Route path="/google-callback" element={<GoogleCallback />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
